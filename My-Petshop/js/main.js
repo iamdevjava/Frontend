04619ce89862a315b681ac9921ef0ce5e85b923e
iamdevjava <apadronv44@gmail.com>
2022-09-19 12:25:01 -0500

    commiteando lo nuevo

---
+++ My-Petshop/js/main.js
@@ -0,0 +1,12 @@
+
+
+function matriz() {
+
+    for(let i = 0; i < matriz.length; i++) {
+        for(let i = 0; i < matriz[i].length; i++) {
+            if(matriz[i][j]%2 === 0) {
+                console.log(matriz[i][j]);
+            }
+        }
+    }
+}
